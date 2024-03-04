6228a7827168ba2fd3272b8456c107cd2efbc190
dominik.halvonik <dominik.halvonik@gmail.com>
2024-03-04 16:14:12 +0100

    POST + routing

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,10 +2,49 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+
 class TestController extends Controller
 {
+    private string $username = "admin";
+    private string $password = "admin";
+
     public function test()
     {
         echo 22;
+    }
+
+    public function testPost(Request $request)
+    {
+        $username = $request->post('username', "fsdafsafds");
+        $password = $request->post('password', "lol");
+        $remember = $request->post('remember', false);
+
+        if($username === $this->username && $password === $this->password) {
+            $response = [
+                'status' => 200,
+                'message' => "Logged in",
+            ];
+
+            return response()->json($response);
+
+        } else {
+            $response = [
+                'status' => 401,
+                'message' => "Incorrect login",
+            ];
+
+            return response()->json($response, $response['status']);
+        }
+    }
+
+    public function testFile(Request $request)
+    {
+        $name = $request->post('meno', "fsdafsafds");
+        $surname = $request->post('priezvisko', "lol");
+
+        file_put_contents("file.txt", json_encode([$name, $surname]));
+
+        return response()->json("juchucu");
     }
 }
