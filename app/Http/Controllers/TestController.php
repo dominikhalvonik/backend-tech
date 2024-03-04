4d3d8b4b027487e7130955fa8ee5406ed1005942
dominik.halvonik <dominik.halvonik@gmail.com>
2024-03-04 17:46:31 +0100

    Routing post

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,10 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+
 class TestController extends Controller
 {
     public function test()
     {
         echo 22;
+    }
+
+    public function testPost(Request $request)
+    {
+        $name = $request->post('meno', null);
+        $surname = $request->post('priezvisko', null);
+        $age = $request->post('vek', 0);
+
+        if($age < 18) {
+            return response()->json("Unable to join under 18", 422);
+        } else {
+            return response()->json([
+                'status' => 200,
+                'message' => $name . " " . $surname . " was correctly assigned",
+            ]);
+        }
+    }
+
+    public function testUpload(Request $request)
+    {
+        $number = $request->post('number', 0);
+        file_put_contents("file.txt", $number);
+
+        return response()->json("juchuchu");
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+Route::post('/test-post', [TestController::class, 'testPost']);
+
+Route::prefix("upload")->group(function () {
+    Route::post('/test-upload', [TestController::class, 'testUpload']);
 });
