9f6f26f9887f9e3f7e51a65c386b4d072d0f24bd
dominik.halvonik <dominik.halvonik@gmail.com>
2024-03-07 09:11:53 +0100

    Add controller

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+
 class TestController extends Controller
 {
     public function test()
@@ -9,8 +11,27 @@ class TestController extends Controller
         echo 22;
     }
 
-    public function random()
+    public function testPost(Request $request)
     {
-        echo mt_rand(1, 5641651);
+        $name = $request->post('meno', null);
+        $surname = $request->post('priezvisko', null);
+        $age = $request->post('vek', 0);
+
+        if($age < 18) {
+            return response()->json("Unable to join under 18", 422);
+        } else {
+            return response()->json([
+                'status' => 200,
+                'message' => $name . " " . $surname . " was correctly assigned",
+            ]);
+        }
+    }
+
+    public function testUpload(Request $request)
+    {
+        $number = $request->post('number', 0);
+        file_put_contents("file.txt", $number);
+
+        return response()->json("juchuchu");
     }
 }
